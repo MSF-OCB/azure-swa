31af03d0f98d9801daf0470093ae6f1c977e31cf
MahmoudAbdo2020 <mahmoud.abdo@brussels.msf.org>
2024-05-12 13:29:36 +0200

    Fix build error

--- pages/project/[path].js
+++ pages/project/[path].js
@@ -22,54 +22,8 @@ function Project({ project }) {
               </li>
             );
           })}
-
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
         </ul>
       </aside>
-      <main>
-        <div className="card-big">
-          <Icon w={249} h={278} />
-          <div className="stats">
-            <div className="stats-details">
-              <div>
-                <StarIcon w={18} h={18} />
-                <p>{project.stargazers_count}</p>
-              </div>
-              <p>stars</p>
-            </div>
-            <div className="stats-details">
-              <div>
-                <WatchIcon w={18} h={18} />
-                <p>{project.subscribers_count}</p>
-              </div>
-              <p>watchers</p>
-            </div>
-            <div className="stats-details">
-              <div>
-                <BugIcon w={18} h={18} />
-                <p>{project.open_issues}</p>
-              </div>
-              <p>issues</p>
-            </div>
-          </div>
-          <p className="description">{project.description}</p>
-          <div className="cta">
-            <a
-              className="button-github"
-              href={project.html_url}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <GithubIcon w={24} h={24} />
-              Learn more...
-            </a>
-          </div>
-        </div>
-      </main>
     </div>
   );
 }
