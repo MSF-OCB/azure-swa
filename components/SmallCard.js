3df15bb87be03a635fe3faada1d9a9e70ff3f11d
MahmoudAbdo2020 <mahmoud.abdo@brussels.msf.org>
2024-05-12 13:18:15 +0200

    Refactor Icons

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function SmallCard({ Icon, title, slug }) {
   return (
-    <a className="card-small" href={`/project/${slug}`}>
-      <Icon w={153} h={163} />
-      <h3>{title}</h3>
-    </a>
+    //<a className="card-small" href={`/${slug}`}>
+    <Icon />
+    // <h3>{title}</h3>
+    // </a>
   );
 }
